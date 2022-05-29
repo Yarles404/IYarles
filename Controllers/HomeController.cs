cbd6ec7906b081ec26cf6ec18e49d5ceba0f75d4
Charles Yang <yarlescy@gmail.com>
2022-05-28 22:40:25 -0500

    contact page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using IYarles.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
+using IYarles.Secrets;
 
 namespace IYarles.Controllers
 {
@@ -30,6 +33,31 @@ namespace IYarles.Controllers
 
         public IActionResult Contact() // TODO: Make it
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contact(ContactEmail model) // TODO: Make it
+        {
+            // print email
+            Console.WriteLine($"Name: {model.Name}, Email: {model.Email}, Message: {model.Message}");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Message = "The form values are invalid, please try again.";
+                ViewBag.Status = "danger";
+                return View();
+            }
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(Secrets.Secrets.YARLES_EMAIL, Secrets.Secrets.GOOGLE_APP_PASSWORD),
+                EnableSsl = true
+            };
+            client.Send("contact@iyarles.net", "yarlescy@gmail.com", $"Message from {model.Name} ({model.Email})", model.GenerateBody());
+
+            ViewBag.Message = "Your message has been sent! I will get back to you as soon as possible.";
+            ViewBag.Status = "primary";
             return View();
         }
 
---
+++ Models/ContactEmail.cs
@@ -0,0 +1,33 @@
+namespace IYarles.Models;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class ContactEmail
+{
+    [Required]
+    [DisplayName("Your Name")]
+    public string Name { get; set; }
+
+    [Required]
+    [DisplayName("Your Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [DisplayName("Message: Please include your orgnization name if applicable")]
+    public string Message { get; set; }
+
+    public ContactEmail(string name, string email, string message)
+    {
+        Name = name;
+        Email = email;
+        Message = message;
+    }
+
+    public ContactEmail() { }
+
+    public string GenerateBody()
+    {
+        return $"Name: {Name}\nEmail: {Email}\n\nMessage: \n{Message}";
+    }
+}
